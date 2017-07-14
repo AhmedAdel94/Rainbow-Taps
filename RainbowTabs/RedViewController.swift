f6df62062ed70ec9f50f3a6f1052dd64ee03674f
Ahmed Adel <AhmedAdel94@users.noreply.github.com>
2017-07-14 13:43:45 +0200

    Add files via upload

---
+++ RainbowTabs/RedViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Ahmed Adel on 6/24/17.
+//  Copyright © 2017 Ahmed Adel. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tabBarItem.badgeValue = nil
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
